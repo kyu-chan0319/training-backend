5a9c63d3dcaed58526e05c27183496a625ca8c32
kyu-chann <yuzuka.m@itnav.co.jp>
2024-01-18 13:27:15 +0900

    add: httpを使用してサーバーの立ち上げる

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,32 @@
-import { serve } from "@hono/node-server";
-import { Hono } from "hono";
+/** Hono ライブラリを使用したサーバーの立ち上げ */
+// import { serve } from "@hono/node-server";
+// import { Hono } from "hono";
 
-const app = new Hono();
+// const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello World!");
+// app.get("/", (c) => {
+//   return c.text("Hello World!");
+// });
+
+// app.get("/hello-world", (c) => {
+//   return c.text("Hello World!");
+// });
+
+// const port = 3000;
+// console.log(`Server is running on port ${port}`);
+
+// serve({
+//   fetch: app.fetch,
+//   port,
+// });
+
+/** Node.js http を使用したサーバーの立ち上げ */
+import * as http from "http";
+
+const port = 4000;
+
+const server = http.createServer((request, response) => {
+  response.end("Hello World!");
 });
 
-app.get("/hello-world", (c) => {
-  return c.text("Hello World!");
-});
-
-const port = 3000;
-console.log(`Server is running on port ${port}`);
-
-serve({
-  fetch: app.fetch,
-  port,
-});
+server.listen(port);
