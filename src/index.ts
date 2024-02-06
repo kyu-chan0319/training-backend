8097525fa1e3bafa216ad47c44c33204eaa0501b
kyu-chann <yuzuka.m@itnav.co.jp>
2024-02-06 15:12:01 +0900

    add: カウンター

--- src/index.ts
+++ src/index.ts
@@ -21,12 +21,39 @@
 // });
 
 /** Node.js http を使用したサーバーの立ち上げ */
+// import * as http from "http";
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// const server = http.createServer((request, response) => {
+//   response.end("Hello World!");
+// });
+
+// // server.listen(port);
+// server.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+/** サーバーにアクセスする度にcounterの値が+1される */
 import * as http from "http";
 
-const port = 4000;
+const port = 3000;
+
+// カウンターの初期値を設定
+let counter = 0;
 
 const server = http.createServer((request, response) => {
-  response.end("Hello World!");
+  // ルートパスの場合のみカウンターを適用
+  if (request.url === "/") {
+    // リクエストが来るたびにカウンターを1増やす
+    counter += 1;
+  }
+  // カウンターの値を表示し、レスポンスを返す
+  response.end(`This page has been accessed ${counter} times.`);
 });
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
